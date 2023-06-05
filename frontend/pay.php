918d08c971201ddf850bfb3acf99beff2e561550
mroxso <mail@pascalrost.de>
2023-06-05 18:03:10 +0200

    calc toPay amount in pay.php

--- frontend/pay.php
+++ frontend/pay.php
@@ -1,11 +1,12 @@
 <?php
 
 $temp_dir = "temp/";
-
 $file = $_GET['file'];
-
+$satsProMb = 50;
 $fileSize = filesize($temp_dir . $file);
+$fileSizeMb = $fileSize / 1000000;
+$toPay = round($fileSizeMb * $satsProMb, 0);
 
-echo $fileSize;
+echo $toPay;
 
 ?>
