cd92ec8421b1574923d919065eb0aaaa0268b0a3
mroxso <mail@pascalrost.de>
2023-06-06 07:50:15 +0200

    add webhook todos

---
+++ frontend/webhook.php
@@ -0,0 +1,10 @@
+<?php 
+
+// TODO:
+// Get filename from path
+// Get invoice from path
+// Check if invoice is paid
+// If paid, move file from temp to storage
+// MAYBE: If not paid, redirect to pay.php
+
+?>
